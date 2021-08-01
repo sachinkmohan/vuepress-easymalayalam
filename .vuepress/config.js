decdb7f8bcac791bc292d3d3e8c9b6d96a45182f
Sachin <sachinkm308@gmail.com>
2021-08-01 23:21:00 +0200

    Added Explained tab and Logs tab

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,40 +1,34 @@
 // config.js
 
-
 module.exports = {
-    title: 'Easy മലയാളം',
-    description: 'Easy Malayalam Website',
+    title: "Easy മലയാളം",
+    description: "Easy Malayalam Website",
     plugins: [
         [
-            '@vuepress/google-analytics',
+            "@vuepress/google-analytics",
             {
-                'ga': 'UA-186989669-1'
-            }
+                ga: "UA-186989669-1",
+            },
         ],
         [
-      	     "vuepress-plugin-google-adsense",
-      	      {
-        	adClient: "ca-pub-8159459534545926", // replace it with your adClient
-      	      }
-    	]
+            "vuepress-plugin-google-adsense",
+            {
+                adClient: "ca-pub-8159459534545926", // replace it with your adClient
+            },
+        ],
     ],
     themeConfig: {
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Blog', link: '/blog/' },
-            { text: 'Mega Quiz', link: '/Mega-Quiz/' },
-            { text: 'Resources-Malayalam', link: '/Resources-Malayalam/' },
-            { text: 'FAQs', link: '/FAQs/' },
-            { text: 'Other-Languages', link: '/Other-Languages/' },
-            { text: 'Small Quizzes', link: '/smallquizzes/' }
+            { text: "Home", link: "/" },
+            { text: "Blog", link: "/blog/" },
+            { text: "Mega Quiz", link: "/Mega-Quiz/" },
+            { text: "Resources-Malayalam", link: "/Resources-Malayalam/" },
+            { text: "Explained", link: "/explained/" },
+            { text: "FAQs", link: "/FAQs/" },
+            { text: "Test", link: "/smallquizzes/" },
+            { text: "Logs", link: "/logs/" },
         ],
 
-
-        sidebar: [
-            '/',
-            '/blog/',
-            '/Mega-Quiz/',
-	    '/FAQs/'
-        ]
-    }
-}
+        sidebar: ["/", "/blog/", "/Mega-Quiz/", "/FAQs/"],
+    },
+};
---
+++ explained/README.md
@@ -0,0 +1,20 @@
+---
+article_index: true
+title: Malayalam doubts clarified | Malayalam Grammar online
+description:
+---
+
+# A place where you can see various doubts clarified
+
+<h3>
+<span style="color:blue">Difference between Isthamulla and Ishtapetta</span>.
+</h3>
+
+![ishtapetta_doubt](../imgs/isthapetta_doubt.jpg)
+
+<h3>
+<span style="color:blue">Difference between Il and Ileku</span>.
+</h3>
+
+![ishtapetta_doubt](../imgs/il_leki.png)
+<ArticleIndex />
