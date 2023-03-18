1ad7400cacb65934117191e9cc826a4df1344013
Sachin Mohan <sachinkm308@gmail.com>
2023-03-18 19:53:41 +0100

    new chatGPT services and more services.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,57 +2,58 @@
 const path = require("path");
 
 module.exports = {
-	title: "Easy മലയാളം",
-	description: "Easy Malayalam Website",
-	plugins: [
-		[
-			"@vuepress/google-analytics",
-			{
-				ga: "UA-186989669-1",
-			},
-		],
-		[
-			"vuepress-plugin-google-adsense",
-			{
-				adClient: "ca-pub-8159459534545926", // replace it with your adClient
-			},
-		],
-		[
-			"@vuepress/register-components",
-			{
-				componentsDir: "./path/to/components",
-			},
-		],
-		[
-			"disqus",
-			{
-				shortname: "https-easymalayalam-fun",
-			},
-		],
-	],
-	themeConfig: {
-		nav: [
-			{ text: "Home", link: "/" },
-			{ text: "Blog", link: "/blog/" },
-			{
-				text: "Quizzes",
-				ariaLabel: "Quiz Menu",
-				items: [
-					{ text: "Mega Quiz", link: "/Mega-Quiz/" },
-					{ text: "Other Quizzes", link: "/quiz/smallquizzes/" },
-				],
-			},
-			{ text: "Resources-Malayalam", link: "/Resources-Malayalam/" },
-			{ text: "Tweets", link: "/twitter/" },
-			{ text: "FAQs", link: "/FAQs/" },
-			{ text: "What's New", link: "/whatsnew/" },
-		],
-	},
-	configureWebpack: {
-		resolve: {
-			alias: {
-				"@alias": path.resolve(__dirname, "../imgs"),
-			},
-		},
-	},
+  title: "Easy മലയാളം",
+  description: "Easy Malayalam Website",
+  plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-186989669-1",
+      },
+    ],
+    [
+      "vuepress-plugin-google-adsense",
+      {
+        adClient: "ca-pub-8159459534545926", // replace it with your adClient
+      },
+    ],
+    [
+      "@vuepress/register-components",
+      {
+        componentsDir: "./path/to/components",
+      },
+    ],
+    [
+      "disqus",
+      {
+        shortname: "https-easymalayalam-fun",
+      },
+    ],
+  ],
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Blog", link: "/blog/" },
+      { text: "Services", link: "/services/" },
+      {
+        text: "Quizzes",
+        ariaLabel: "Quiz Menu",
+        items: [
+          { text: "Mega Quiz", link: "/Mega-Quiz/" },
+          { text: "Other Quizzes", link: "/quiz/smallquizzes/" },
+        ],
+      },
+      { text: "Resources-Malayalam", link: "/Resources-Malayalam/" },
+      { text: "Tweets", link: "/twitter/" },
+      { text: "FAQs", link: "/FAQs/" },
+      { text: "What's New", link: "/whatsnew/" },
+    ],
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@alias": path.resolve(__dirname, "../imgs"),
+      },
+    },
+  },
 };
---
+++ services/README.md
@@ -0,0 +1,20 @@
+---
+title: Services offered to learn and improve Malayalam language
+description: All the services offered by Easy Malayalam team will appear here.
+---
+
+# Services
+
+Welcome to the services offered by Easy Malayalam
+
+## ChatGPT Services
+
+1.  **1-1** session to get started with 'Introduction of ChatGPT on Malayalam' on a Zoom call where we will learn how to use and interact with the platform and learn to generate translations. Additionaly, you will learn how to ask for more examples, learn more grammar etc. Finally, you will also be learning how to create flashcards for learning Malayalam quicky. If you choose this, proceed here -> [Link](https://www.buymeacoffee.com/easymalayalam/e/123147)
+
+2.  If you know already how to use ChatGPT, then opt for this option. Here we can discuss the translations(only by exchanging text) on Discord server at your own pace. If you choose this, proceed here -> [Link](https://www.buymeacoffee.com/easymalayalam/e/123149)
+
+## General Services
+
+1. Chat with me on my discord server, where you can ask 10 casual questions. For eg, some simple translations like 'what do you say house in Malayalam?' Or maybe ask for a movie suggestion. To opt for this service, proceed here -> [Link](https://www.buymeacoffee.com/easymalayalam/e/124605)
+
+2. Ask me 10 Malayalam(for eg. Grammar or culture related, etc.) questions(your choice) to get clarified. To opt for this service, proceed here -> [Link](https://www.buymeacoffee.com/easymalayalam/e/124606)
